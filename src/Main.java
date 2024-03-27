94816cfe75902c19858168eb586c1b8163c6ef4b
Samsel Szymon <G115@zsz3.local>
2024-03-27 09:59:37 +0100

    Dodano mapę

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Miejsce[] mapa = new Miejsce[7];
+		
+		mapa[0] = new Miejsce("Start");
+		mapa[1] = new Miejsce("M1");
+		mapa[2] = new Miejsce("M2");
+		mapa[3] = new Miejsce("M3");
+		mapa[4] = new Miejsce("M4");
+		mapa[5] = new Miejsce("M5");
+		mapa[6] = new Miejsce("Koniec");
+		
+		mapa[0].addEntry(null, mapa[1], null, null);
+		mapa[1].addEntry(mapa[0], mapa[1], null, mapa[2]);
+		mapa[2].addEntry(null, null, mapa[1], mapa[3]);
+		mapa[3].addEntry(null, mapa[5], mapa[2], mapa[4]);
+		mapa[4].addEntry(null, null, mapa[3], null);
+		mapa[5].addEntry(mapa[3], mapa[6], null, null);
+		mapa[6].addEntry(null, mapa[5], null, null);
+		
+		
+		Miejsce meRn = mapa[0];
+		Scanner scan = new Scanner(System.in);
+		
+		while(!meRn.equals(mapa[6])) {
+			System.out.println("Jesteś w "+meRn.getName());
+			System.out.println("Możesz iść do: \n -"+meRn.returnEntry());
+		}
+	}
+
+}
---
+++ src/Miejsce.java
@@ -0,0 +1,40 @@
+
+public class Miejsce {
+	protected String name;
+	protected String desc;
+	protected Miejsce[] przejscia = new Miejsce[4];
+	
+	public Miejsce(String name) {
+		this.name=name;
+	}
+	
+	public void addEntry(Miejsce w, Miejsce s, Miejsce a, Miejsce d) {
+		przejscia[0]=w;
+		przejscia[1]=s;
+		przejscia[2]=a;
+		przejscia[3]=d;
+	}
+	public String returnEntry() {
+		String door = "";
+		if(przejscia[0]!=null) door+="przodu: ";
+		if(przejscia[1]!=null) door+="tyłu: ";
+		if(przejscia[2]!=null) door+="w lewo: ";
+		if(przejscia[3]!=null) door+="w prawo: ";
+		return door;
+	}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+}
